83e988d68fd6c335a11d551f7ea674b5c4f889ae
Nathan Gibbs <nathan@cmpublishers.com>
2019-02-15 06:40:15 +0000

    20190215 Build fix #2

--- tests/setphpunit.sh
+++ tests/setphpunit.sh
@@ -12,7 +12,7 @@ pvm=`echo $puv|sed -r -e "s/^[0-9]\.//" -e "s/\.[0-9]+$//"`
 pvr=`echo $puv|sed -r -e "s/^[0-9]\.[0-9]\.//"`
 
 echo "System PHPUnit Version: $puv"
-if [ "$pvM" == "4" ] && [ "$pvm" == "8" ] && [ "$pvr" < "28" ]; then
+if [ "$pvM" == "4" ] && [ "$pvm" == "8" ] && [ "$pvr" \< "28" ]; then
 	echo "Using Composer PHPUnit"
 	if [ "$TRAVIS" == "true" ]; then
 		export ComPU=1
