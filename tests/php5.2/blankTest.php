932f3bea5273a36a84ea5a5026548d1b1d3c5b23
Nathan Gibbs <nathan@cmpublishers.com>
2019-02-15 02:59:19 +0000

    20190214 Build fix #4

---
+++ tests/php5.2/blankTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class BlankTest extends PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+        // Optional: Test anything here, if you want.
+        $this->assertTrue(true, 'This should already work.');
+
+        // Stop here and mark this test as incomplete.
+        $this->markTestIncomplete(
+          'Placeholder Blank Test.'
+        );
+    }
+}
+
+?>
