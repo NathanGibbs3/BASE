9ac1f5af56cf55cc613daf0802da0a286a35db96
Nathan Gibbs <nathan@cmpublishers.com>
2019-02-17 23:48:57 +0000

    20190217 Build fix 11 PHP 5.2 XDebug enable 8

--- tests/setphpunit.sh
+++ tests/setphpunit.sh
@@ -6,14 +6,18 @@ else
 	echo "Not Running on Travis-CI"
 fi
 
+pu=phpunit
+syspu=`which $pu`
 puv=`phpunit --version|sed -e "s/^PHPUnit\s//" -e "s/\sby.*$//"`
 pvM=`echo $puv|sed -r -e "s/\.[0-9]\.[0-9]+$//"`
 pvm=`echo $puv|sed -r -e "s/^[0-9]\.//" -e "s/\.[0-9]+$//"`
 pvr=`echo $puv|sed -r -e "s/^[0-9]\.[0-9]\.//"`
 
 echo "System PHPUnit Version: $puv"
+echo "              Location: $syspu"
 if [ "$pvM" == "4" ] && [ "$pvm" == "8" ] && [ "$pvr" \< "28" ]; then
 	echo "Using Composer PHPUnit"
+	pu="vendor/bin/$pu"
 	if [ "$TRAVIS" == "true" ]; then
 		export ComPU=1
 	fi
@@ -22,4 +26,13 @@ else
 	if [ "$TRAVIS" == "true" ]; then
 		export ComPU=0
 	fi
+fi
+if [ "$XDebug" == "1" ]; then
+	pux="php -d xdebug.profiler_enable=on $pu"
+else
+	pux=$pu
+fi
+echo "PHPUnit Executable: $pux"
+if [ "$TRAVIS" == "true" ]; then
+	export PUX=$pux
 fi
