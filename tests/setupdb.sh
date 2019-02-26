7cf226a6ffc51d715eb69ffeba47f6ca2d5ab137
Nathan Gibbs <nathan@cmpublishers.com>
2019-02-26 02:52:40 +0000

    20190225 Build Fix 4 Fix for Issue #3 Attempt 1

--- tests/setupdb.sh
+++ tests/setupdb.sh
@@ -11,6 +11,15 @@ if [ "$DB" = "postgres" ]; then
 		echo "Creating $DB Database snort"
 		psql -c 'DROP DATABASE IF EXISTS snort;' -U postgres
 		psql -c 'CREATE DATABASE snort;' -U postgres
+		echo "Pulling $DB SNORT Table creation scripts from"
+		echo "https://github.com/firnsy/barnyard2"
+		wget https://github.com/firnsy/barnyard2/blob/master/schemas/create_postgresql
+		if [ -a ./create_postgresql ]; then
+			echo "Creating SNORT Tables"
+			psql -d snort -f ./create_postgresql
+		else
+			echo "Not creating SNORT Tables, Download failed"
+		fi
 		echo "Creating BASE Tables"
 		psql -d snort -f sql/create_base_tbls_pgsql.sql
 		echo "Adding referential integrity to the database schema"
@@ -20,8 +29,17 @@ elif [ "$DB" = "mysql" ]; then
 	if [ "$TRAVIS" == "true" ]; then
 		echo "Creating $DB Database snort"
 		mysql -e 'CREATE DATABASE IF NOT EXISTS snort;'
+		echo "Pulling $DB SNORT Table creation scripts from"
+		echo "https://github.com/firnsy/barnyard2"
+		wget https://github.com/firnsy/barnyard2/blob/master/schemas/create_mysql
+		if [ -a ./create_mysql ]; then
+			echo "Creating SNORT Tables"
+			mysql -D snort < ./create_mysql
+		else
+			echo "Not creating SNORT Tables, Download failed"
+		fi
 		echo "Creating BASE Tables"
-		mysql snort < sql/create_base_tbls_mysql.sql
+		mysql -D snort < sql/create_base_tbls_mysql.sql
 	fi
 else
 	echo "Not Setting up Database"
