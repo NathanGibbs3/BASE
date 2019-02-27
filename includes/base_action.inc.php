NathanGibbs3/BASE

commit dd441fd90ef693c5c9469372ae372d54485a94b4

20190226 Fix Mariadb compatibility 1 Usage of `ref_system_id` column :smile: Used concept from firnsy/barnyard2#186

includes/base_signature.inc.php
@@ -90,7 +90,7 @@ function GetRefSystemName($ref_system_id, $db)
 
    $ref_system_name = "";
 
-   $tmp_sql = "SELECT ref_system_name FROM reference_system WHERE ref_system_id='".$ref_system_id."'";
+	$tmp_sql = "SELECT ref_system_name FROM reference_system WHERE `ref_system_id`='".$ref_system_id."'";
    $tmp_result = $db->baseExecute($tmp_sql);
    if ( $tmp_result )
    {
@@ -231,7 +231,7 @@ function GetSignatureReference($sig_id, $db, $style)
          {
             $mysig_ref = $tmp_sig_ref->baseFetchRow();
    
-            $temp_sql = "SELECT ref_system_id, ref_tag FROM reference WHERE ref_id='".$mysig_ref[1]."'";
+			$temp_sql = "SELECT `ref_system_id`, ref_tag FROM reference WHERE ref_id='".$mysig_ref[1]."'";
             $tmp_ref_tag = $db->baseExecute($temp_sql);
    
             if ( $tmp_ref_tag )

commit 8924f95f60689d8adc839427aa446e9479864dde

20190226 Fix for Mariadb & PostgreSQL interoperability 1 Commit dd441fd9 broke PostgreSQL support. :frown: Learned that from reading firnsy/barnyard2#193

includes/base_action.inc.php
@@ -1067,9 +1067,11 @@ function Action_archive_alert_op($sid, $cid, &$db, $action_arg, &$ctx)
          if (isset($tmp_row) && !empty($tmp_row))
          {
            $ref_id = $tmp_row[0];
-
-           $sql = "SELECT ref_system_id, ref_tag FROM reference ".
-                  "WHERE ref_id='".$ref_id."'";
+			if ( $db->DB_class == 1 ) { /* Mysql & MariaDB */
+				$sql = "SELECT `ref_system_id`, ref_tag FROM reference WHERE ref_id='".$ref_id."'";
+			}else{ /* Everyone else */
+				$sql = "SELECT ref_system_id, ref_tag FROM reference WHERE ref_id='".$ref_id."'";
+			}
            $tmp_result2 = $db->baseExecute($sql);
            $tmp_row2 = $tmp_result2->baseFetchRow();   
            $tmp_result2->baseFreeRows();
@@ -1222,8 +1224,11 @@ function Action_archive_alert_op($sid, $cid, &$db, $action_arg, &$ctx)
      for ( $j = 0; $j < $sig_reference_cnt; $j++ )
      {
         /* get the ID of the reference system */
-        $tmp_sql = "SELECT ref_system_id FROM reference_system WHERE ".
-                   "ref_system_name = '".$sig_reference[$j][2]."'";
+		if ( $db->DB_class == 1 ) { /* Mysql & MariaDB */
+			$tmp_sql = "SELECT `ref_system_id` FROM reference_system WHERE ref_system_name = '".$sig_reference[$j][2]."'";
+		}else{ /* Everyone else */
+			$tmp_sql = "SELECT ref_system_id FROM reference_system WHERE ref_system_name = '".$sig_reference[$j][2]."'";
+		}
         $tmp_result = $db2->baseExecute($tmp_sql);
         $tmp_row = $tmp_result->baseFetchRow();
         $tmp_result->baseFreeRows();
@@ -1249,11 +1254,11 @@ function Action_archive_alert_op($sid, $cid, &$db, $action_arg, &$ctx)
         {
            $ref_system_id = -1;
         }
-
-        $sql = "SELECT ref_id FROM reference WHERE ".
-               "ref_system_id='".$ref_system_id."' AND ".
-               "ref_tag='".$sig_reference[$j][1]."'";
-
+		if ( $db->DB_class == 1 ) { /* Mysql & MariaDB */
+			$sql = "SELECT ref_id FROM reference WHERE `ref_system_id`='".$ref_system_id."' AND ref_tag='".$sig_reference[$j][1]."'";
+		}else{ /* Everyone else */
+			$sql = "SELECT ref_id FROM reference WHERE ref_system_id='".$ref_system_id."' AND ref_tag='".$sig_reference[$j][1]."'";
+		}
         if ($db->DB_type == "mssql")
         {
            /* MSSQL doesn't allow "=" with TEXT data types, but it does
@@ -1273,12 +1278,12 @@ function Action_archive_alert_op($sid, $cid, &$db, $action_arg, &$ctx)
         {
            $ref_id = $tmp_row[0];
            $tmp_result->baseFreeRows();
-        }
-        else
-        {
-           $sql = "INSERT INTO reference (ref_system_id, ref_tag) ".
-                  " VALUES (".$sig_reference[$j][0].",'".
-                              $sig_reference[$j][1]."')";
+		}else{
+			if ( $db->DB_class == 1 ) { /* Mysql & MariaDB */
+				$sql = "INSERT INTO reference (`ref_system_id`, ref_tag) VALUES (".$sig_reference[$j][0].",'".$sig_reference[$j][1]."')";
+			}else{ /* Everyone else */
+				$sql = "INSERT INTO reference (ref_system_id, ref_tag) VALUES (".$sig_reference[$j][0].",'".$sig_reference[$j][1]."')";
+			}
            $db2->baseExecute($sql);
            $ref_id = $db2->baseInsertID(); 
 

includes/base_db.inc.php
@@ -683,11 +683,11 @@ function NewBASEDBConnection($path, $type)
       include($path."\\adodb.inc.php");
    }
 
-	/* Under PHP 7.x, use mysqli ADODB driver & gracefully deprecate the mysql
+	/* Under PHP 7+, use mysqli ADODB driver & gracefully deprecate the mysql
 	& mysqlt drivers. */
 	if ( $type == "mysql" || $type == "mysqlt" ) {
 		$version = explode( ".", phpversion() );
-		if ( $version[0] == 7 ) {
+		if ( $version[0] >= 7 ) {
 			$type = "mysqli";
 		}
 	}

includes/base_signature.inc.php
@@ -90,7 +90,11 @@ function GetRefSystemName($ref_system_id, $db)
 
    $ref_system_name = "";
 
-	$tmp_sql = "SELECT ref_system_name FROM reference_system WHERE `ref_system_id`='".$ref_system_id."'";
+	if ( $db->DB_class == 1 ) { /* Mysql & MariaDB */
+		$tmp_sql = "SELECT ref_system_name FROM reference_system WHERE `ref_system_id`='".$ref_system_id."'";
+	}else{ /* Everyone else */
+		$tmp_sql = "SELECT ref_system_name FROM reference_system WHERE ref_system_id='".$ref_system_id."'";
+	}
    $tmp_result = $db->baseExecute($tmp_sql);
    if ( $tmp_result )
    {
@@ -230,8 +234,11 @@ function GetSignatureReference($sig_id, $db, $style)
          for ( $i = 0; $i < $num_references; $i++)
          {
             $mysig_ref = $tmp_sig_ref->baseFetchRow();
-   
+		if ( $db->DB_class == 1 ) { /* Mysql & MariaDB */
 			$temp_sql = "SELECT `ref_system_id`, ref_tag FROM reference WHERE ref_id='".$mysig_ref[1]."'";
+		}else{ /* Everyone else */
+			$temp_sql = "SELECT ref_system_id, ref_tag FROM reference WHERE ref_id='".$mysig_ref[1]."'";
+		}
             $tmp_ref_tag = $db->baseExecute($temp_sql);
    
             if ( $tmp_ref_tag )