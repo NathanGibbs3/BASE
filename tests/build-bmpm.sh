ab5d1e40edf2a4f97cd4e6fdb196c1574cca1d1b
Nathan Gibbs <nathan@cmpublishers.com>
2019-03-06 00:14:02 +0000

    20190305 Build Fix 1 Code Coverage 1

--- tests/build-bmpm.sh
+++ tests/build-bmpm.sh
@@ -10,11 +10,34 @@ td=`pwd|sed -e "s/^.*\///"`
 OM=/usr/share/ieee-data/oui.txt
 PF=base_mac_prefixes.map
 
+if [ ! -f $OM ]; then
+	echo "No System Mac Prefix DB"
 	if [ "$td" == "tests" ]; then
-		OF="./bmpm.tmp"
+		OM=./oui.txt
+	elif [ "$td" == "BASE" ]; then
+		OM=./tests/oui.txt
+	else
+		OM=''
+	fi
+	if [ "$OM" != "" ]; then
+		echo "Pulling DB from"
+		echo "http://standards-oui.ieee.org"
+		wget -nv http://standards-oui.ieee.org/oui/oui.txt
+		echo "Using Local Mac Prefix DB"
+	else
+		echo "Not Running in Local test environment"
+		echo "Exiting"
+		exit
+	fi
+else
+	echo "Using System Mac Prefix DB"
+fi
+
+	if [ "$td" == "tests" ]; then
+		OF="./bmpm"
 	elif [ "$td" == "BASE" ]; then
 		if [ "$TRAVIS" != "true" ]; then
-			OF="./tests/bmpm.tmp"
+			OF="./tests/bmpm"
 		else
 			OF="./$PF"
 		fi
@@ -23,11 +46,27 @@ PF=base_mac_prefixes.map
 	fi
 
 if [ -a $OM ]; then
+	# Sorted MAC List
+	cat $OM | egrep "^[[:xdigit:]]{6} {5}\(base 16\)\s{2}"|sort > "$OF.tmp0"
+	# Global sed transforms
+	cat "$OF.tmp0" |sed -r -e "s/\s+\(base 16\)\s+/  /" -e "s/( +)?\r$//" -e "s/ +\././" > "$OF.tmp1"
+	# Initial Upper Case Vendor Names
+	cat "$OF.tmp1" |sed -r -e "s/\b[A-Z]{2,}/\L\0/g" -e "s/\b./\u\0/g" > "$OF.tmp0"
+	# Standardize Abbreviations
+	cat "$OF.tmp0" |sed -r -e "s/Gmbh/GmbH/" > "$OF.tmp1"
+	# Specific Vendor Name Transforms
+	cat "$OF.tmp1" |sed -r -e "s/Cisco Systems,? Inc\.?/Cisco Systems Inc./" > "$OF.tmp0"
+	cat "$OF.tmp0" |sed -r -e "s/Hewlett Packard/Hewlett-Packard Company/" > "$OF.tmp1"
+	cat "$OF.tmp1" |sed -r -e "s/3COM/3Com/" > "$OF.tmp0"
 	if [ "$OF" == "" ]; then
-		cat $OM | egrep "^[[:xdigit:]]{6}"|sort|sed -r -e "s/\s+\(base 16\)\s+/  /" -r -e "s/ +?\r$//" -r -e "s/\b[A-Z]{2,}/\L\0/g" -e "s/\b./\u\0/g"
+		cat "$OF.tmp0"
 	else
-		echo "Rebuilding $PF"
-		cat $OM | egrep "^[[:xdigit:]]{6}"|sort|sed -r -e "s/\s+\(base 16\)\s+/  /" -r -e "s/ +?\r$//" -r -e "s/\b[A-Z]{2,}/\L\0/g" -e "s/\b./\u\0/g" > $OF
+		if [ "$TRAVIS" == "true" ]; then
+			echo "Rebuilding $PF"
+		else
+			echo "Writing $OF.txt"
+		fi
+		cat "$OF.tmp0" > "$OF.txt"
 	fi
 else
 	echo "System Mac Prefix DB does not exist"
