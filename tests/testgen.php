4b3fe664015f40bde5a98ad3555c70c28096aac8
Nathan Gibbs <nathan@cmpublishers.com>
2019-04-29 01:11:38 +0000

    20190428 Automated PHPUnit Test generation.
    Added automated PHPUnit Test generation for the PHP 5.2x
    Non-Namespaced PHPUnit Test Class.
    Testing this on branch travis-ci before merging.
    For concept validation, re-enabled PHP 5.2 Testsing in CI config.
    Build enhancements added from branches devel & Issue11.
    Added composer.sh fix from:
    Commit: 2d4c65d5654142d2c01f1ca9734040d3a3c0e89f
    Added .travis.yml fix from:
    Commit: 923d0b149c9e5ee87a904bbc025c90514ff4b871

---
+++ tests/testgen.php
@@ -0,0 +1,140 @@
+<?php
+
+// Purpose: Generate tests for multiple PHPUnit Versions
+// In the Issue11 branch we now have tests for 3 different iterations of
+// PHPUnit. 90% or more of the code in each iteration is identical. What
+// differs are the details in:
+//	Class defines Namespaced/Non-Namespaced.
+//	Fixture defines Typehinted/Non-Typehinted.
+// This process would be less error prone if automated.
+
+$Mts = 'php'; // Master Test Set.
+$debug_mode = 0;
+
+$Mts = "tests/$Mts";
+$tfl = testfiles($Mts); // Master Test List.
+$tsl = testsuites(); // Test sets to generate.
+
+if ($tfl == NULL) {
+	print 'No Test Files. Exiting.';
+	exit;
+}elseif ($tsl == NULL) {
+	print 'No Test Suites. Exiting.';
+	exit;
+}else{
+	print "Master Test Set: $Mts";
+	$Mtc = preg_replace("/\//","\/",$Mts); // Escape Pathsep.
+	foreach($tsl as $gts){ // Process Test Sets.
+		if (preg_match("/^".$Mtc."$/", $gts) ) {
+			// Drop Master Test Set.
+			print "\nDropping Master Test Set: $gts\n";
+		}else{
+			$tfwflag = 0;
+			$dts = preg_replace( "/tests\//", "", $gts);
+			print "\n Build Test Set: $dts";
+			print "\n\tOpts: (Namespace: ";
+			if(preg_match("/tests\/php5.2/", $gts)) {
+				print "No Typehint: No";
+				$tfwflag = 1;
+			}elseif(preg_match("/tests\/php7.1/", $gts)) {
+				print "Yes Typehint: Yes";
+			}else{
+				print "Yes Typehint: No";
+			}
+			print ')';
+			foreach($tfl as $gtf){ // Process Test files.
+				$tfc = file("$Mts/$gtf");
+				$dtf = preg_replace( "/\.php/", "", $gtf);
+				$gtfn = "$gts/$gtf";
+				print "\n\tTest: $dtf";
+				$ntfc = array();
+				foreach ($tfc as $i => $line) { // Process Test File.
+					if(preg_match("/tests\/php5.2/", $gts)) {
+						if(!preg_match( // Drop use line.
+							"/^use PHPUnit\\\Framework\\\TestCase;$/",
+							$line
+						)) {
+							$line = preg_replace( // Non-Namespace Test Class.
+								"/extends TestCase/",
+								"extends PHPUnit_Framework_TestCase",
+								$line
+							);
+							array_push($ntfc,$line);
+						}
+					}elseif(preg_match("/tests\/php7.1/", $gts)) {
+					}else{
+					}
+				}
+				if ( $tfwflag == 1 ) {
+					$tmp = implode('', $ntfc);
+					if (!$handle = fopen($gtfn, 'w')) {
+						print "Cannot open file ($gtf)";
+						exit;
+					}
+					if (fwrite($handle, $tmp) === FALSE) {
+						print "Cannot write to file ($filename)";
+						exit;
+					}
+					fclose($handle);
+				}else{
+					print_r($tfc);
+					print_r($ntfc);
+					print "Would write test file: $gtfn";
+				}
+			}
+		}
+	}
+}
+
+function testfiles($path) { // Returns array of testfiles
+	GLOBAL $debug_mode;
+	$ll = array();
+	$path .= '/';
+	$prefix = "$path*Test.php";
+	$files = glob("$prefix");
+	if(is_array($files) && !empty($files)){
+		if ($debug_mode > 1) {
+			print "\nThe following testfiles were found:";
+		}
+		$path = preg_replace("/\//","\/",$path);
+		foreach($files as $match){
+			$match = preg_replace( "/$path/", "", $match);
+			$ll[]=$match;
+			if ($debug_mode > 1) {
+				print "\n$match";
+			}
+		}
+	}else{
+		$ll = NULL;
+		if ($debug_mode > 1) {
+			print "\nEmpty testsfile List";
+		}
+	}
+	return $ll;
+}
+
+function testsuites() { // Returns array of testsuites.
+	GLOBAL $debug_mode;
+	$ll = array();
+	// Match "tests/php#.#
+	$prefix = "tests/php*[0-9].*[0-9]";
+	$files = glob("$prefix");
+	if(is_array($files) && !empty($files)){
+		if ($debug_mode > 1) {
+			print "\nThe following testsuites were found:";
+		}
+		foreach($files as $match){
+			$ll[]=$match;
+			if ($debug_mode > 1) {
+				print "\n$match";
+			}
+		}
+	}else{
+		$ll = NULL;
+		if ($debug_mode > 1) {
+			print "\nEmpty testsuite List";
+		}
+	}
+	return $ll;
+}
+?>
