af942b9e6d62b71cdd8ad0c0a9d9f06c412c7b02
Nathan Gibbs <nathan@cmpublishers.com>
2019-06-02 01:48:59 +0000

    20190601 Issue #34 Build Cleanup 2

--- tests/phpunit.sh
+++ tests/phpunit.sh
@@ -19,37 +19,31 @@ if [ "$TRAVIS" != "true" ]; then
 		exit
 	fi
 fi
-if [ "$SafeMode" = "1" ]; then
-	puv=`$ph vendor/bin/$pu --version`
-else
-	puv=`$pu --version`
-fi
-puv=`echo $puv|sed -e "s/^PHPUnit\s//" -e "s/\sby.*$//"`
-pvM=`echo $puv|sed -r -e "s/\.[0-9]\.[0-9]+$//"`
-pvm=`echo $puv|sed -r -e "s/^[0-9]\.//" -e "s/\.[0-9]+$//"`
-pvr=`echo $puv|sed -r -e "s/^[0-9]\.[0-9]\.//"`
-if [ "$SafeMode" != "1" ]; then
-	echo -n "  System"
-else
-	echo -n "Composer"
-fi
-echo " PHPUnit Version: $puv"
-if [ "$TRAVIS" != "true" ]; then
-	echo "                Location: `which $pu`"
-fi
 # Use Composer installed or System version?
-if [ "$SafeMode" = "1" ] || (
-	[ "$pvM" == "4" ] && [ "$pvm" == "8" ] && [ "$pvr" \< "19" ]
-); then
-	# Apparantly PHPUnits below 4.8.19 fail with a
-	# PHP Fatal error: Class 'Text_Template' not found
-	# when using expectOutputString in test code.
-	# This may be a Debian/Ubuntu specific Issue.
+if [ "$SafeMode" = "1" ]; then
 	pi=Composer
 	px="$ph vendor/bin/$pu"
 else
-	pi=System
-	px=$pu
+	puv=`$pu --version`
+	puv=`echo $puv|sed -e "s/^PHPUnit\s//" -e "s/\sby.*$//"`
+	pvM=`echo $puv|sed -r -e "s/\.[0-9]\.[0-9]+$//"`
+	pvm=`echo $puv|sed -r -e "s/^[0-9]\.//" -e "s/\.[0-9]+$//"`
+	pvr=`echo $puv|sed -r -e "s/^[0-9]\.[0-9]\.//"`
+	echo "System PHPUnit Version: $puv"
+	if [ "$TRAVIS" != "true" ]; then
+		echo "                Location: `which $pu`"
+	fi
+	if [ "$pvM" == "4" ] && [ "$pvm" == "8" ] && [ "$pvr" \< "19" ]; then
+		# Apparantly PHPUnits below 4.8.19 fail with a
+		# PHP Fatal error: Class 'Text_Template' not found
+		# when using expectOutputString in test code.
+		# This may be a Debian/Ubuntu specific Issue.
+		pi=Composer
+		px="$ph vendor/bin/$pu"
+	else
+		pi=System
+		px=$pu
+	fi
 fi
 # Now what PHPUnit Version are we using?
 puv=`$px --version|sed -e "s/^PHPUnit\s//" -e "s/\sby.*$//"`
