c4f676d40fc9f8586fa0b08297a114987335ceac
Nathan Gibbs <nathan@cmpublishers.com>
2019-06-06 18:30:40 +0000

    Revert "20190605 Issue #11 Build fix 2"
    This reverts commit 7b7f194cae38d9856153d2c1873b204b913b9f12.

--- tests/php/langtdflegacyTest.php
+++ tests/php/langtdflegacyTest.php
@@ -54,12 +54,10 @@ class legacylangTest extends TestCase {
 		$PHPV = GetPHPV();
 		if (
 			version_compare($PHPV, '5.2', '>')
-//			&& ini_get("safe_mode") == true
+			&& ini_get("safe_mode") == true
 		){
 			// Turn off safe mode.
-			if ( ini_set('safe_mode','0') === false){
-				self::markTestIncomplete('PHP SafeMode: On');
-			}
+			ini_set("safe_mode",0);
 		}
 	}
 	public static function tearDownAfterClass() {
