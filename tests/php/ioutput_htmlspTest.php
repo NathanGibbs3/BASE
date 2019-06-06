3f8bc5e522d9c63b2ad753025c93b56305d06411
Nathan Gibbs <nathan@cmpublishers.com>
2019-06-06 18:30:40 +0000

    Revert "20190605 Issue #11 Build fix 13"
    This reverts commit 787dad9200323ffd3a57de8c53807a8e122d7689.

--- tests/php/ioutput_htmlspTest.php
+++ tests/php/ioutput_htmlspTest.php
@@ -67,13 +67,13 @@ class output_htmlSPTest extends TestCase {
 			$this->markTestSkipped('Requires Phpunit 4+ to run.');
 		}elseif (version_compare($PHPUV, '5.0', '<')) { // PHPUnit 4x
 			$this->setExpectedException(
-				"PHPUnit_Framework_Error_Error", $EEM
+				"PHPUnit_Framework_Error_Notice", $EEM
 			);
 		}elseif (version_compare($PHPUV, '6.0', '<')) { // PHPUnit 5x
-			$this->expectException("PHPUnit_Framework_Error");
+			$this->expectException("PHPUnit_Framework_Error_Notice");
 			$this->expectExceptionMessage($EEM);
 		}else{ // PHPUnit 6+
-			$this->expectException("PHPUnit\Framework\Error\Error");
+			$this->expectException("PHPUnit\Framework\Error\Notice");
 			$this->expectExceptionMessage($EEM);
 		}
 
