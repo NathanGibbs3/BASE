88cc6b5bfb6124fb0943cf167e01069ab24869a0
Nathan Gibbs <nathan@cmpublishers.com>
2019-06-08 01:15:28 +0000

    20190606 Issue #5 Work 2          Modified PHP 5.2 custom base_conf.

--- tests/phpcommon/5.2-base_conf.php
+++ tests/phpcommon/5.2-base_conf.php
@@ -1,8 +1,17 @@
 <?php
 
-// Fix for Issue #5.
-// Travis-CI PHP 5.2 Minimal base_conf.php
+// Fix for Issue #5 Code Coverage Warnings.
+// Travis-CI PHP 5.2 Custom base_conf.php
 $BASE_path = dirname(__FILE__);
-define( '_BASE_INC', 1 );
-
+$TRAVIS = getenv('TRAVIS');
+if ($TRAVIS){ // Running o
+	$ADO = getenv('ADODBPATH');
+	$DBlib_path = "build/adodb/$ADO";
+	$DB = getenv('DB');
+	if ($DB == 'mysql' ){
+		require('./tests/phpcommon/DB.mysql.php');
+	}elseif ($DB == 'postgres' ){
+		require('./tests/phpcommon/DB.pgsql.php');
+	}
+}
 ?>
