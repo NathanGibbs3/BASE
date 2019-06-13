1b13a7b9171746b51cad5be99cebb4502f896e80
Nathan Gibbs <nathan@cmpublishers.com>
2019-06-13 01:59:40 +0000

    20190612 Issue #5 Work 5          All include(s) for base_graph_common.php          Are now include_once()

--- base_graph_display.php
+++ base_graph_display.php
@@ -35,7 +35,7 @@
   include ("base_conf.php");
 include_once ("$BASE_path/includes/base_constants.inc.php");
 include_once ("$BASE_path/includes/base_state_common.inc.php");
-  include ("$BASE_path/base_graph_common.php");
+include_once ("$BASE_path/base_graph_common.php");
   require_once('Image/Graph.php');
   
   
