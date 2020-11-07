ba68511c620db38e9cab3102b1fad762cbf44d52
Nathan Gibbs <nathan@cmpublishers.com>
2020-11-07 03:32:34 +0000

    20201106 Build Fix 1

--- tests/php/base_commonspTest.php
+++ tests/php/base_commonspTest.php
@@ -12,25 +12,32 @@ class base_commonSPTest extends TestCase {
 	// Tests go here.
 	public function testreturnChkAccessReadFail() {
 		GLOBAL $BASE_path;
-		$sc = DIRECTORY_SEPARATOR;
-		$file = 'readTestFail.txt';
-		$Testfile = "$BASE_path$sc" . "custom$sc$file";
+		// Issue #36 Cutout.
+		// See: https://github.com/NathanGibbs3/BASE/issues/36
 		$PHPV = GetPHPV();
-		if ( posix_getuid() != 1000 ){ // Swith UID to test Read Failure.
-			posix_setuid(1000);
-		}
-		$expected = -2;
-		if (
+		$PSM = getenv('SafeMode');
+		if (version_compare($PHPV, '5.4', '<') && $PSM == 1){
+			$this->markTestSkipped();
+		}else{
+			$sc = DIRECTORY_SEPARATOR;
+			$file = 'readTestFail.txt';
+			$Testfile = "$BASE_path$sc" . "custom$sc$file";
+			if ( posix_getuid() != 1000 ){ // Swith UID to test Read Failure.
+				posix_setuid(1000);
+			}
+			$expected = -2;
+			if (
 				version_compare($PHPV, '5.1.5', '<')
 				&& ini_get("safe_mode") == true
 			){ // Safe Mode Cutout Return Value
 				$expected = 1;
+			}
+			$this->assertEquals(
+				$expected,
+				ChkAccess($Testfile),
+				'Unexpected return ChkAccess().'
+			);
 		}
-		$this->assertEquals(
-			$expected,
-			ChkAccess($Testfile),
-			'Unexpected return ChkAccess().'
-		);
 	}
 
 	// Add code to a function if needed.
