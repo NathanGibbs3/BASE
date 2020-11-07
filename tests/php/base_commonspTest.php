3fda49aac87c1b6934b81de9a6ca05ada2d3fda0
Nathan Gibbs <nathan@cmpublishers.com>
2020-11-07 03:47:56 +0000

    20201106 Build Fix 2

--- tests/php/base_commonspTest.php
+++ tests/php/base_commonspTest.php
@@ -9,35 +9,43 @@ use PHPUnit\Framework\TestCase;
   * @runTestsInSeparateProcesses
   */
 class base_commonSPTest extends TestCase {
-	// Tests go here.
-	public function testreturnChkAccessReadFail() {
-		GLOBAL $BASE_path;
+	// Pre Test Setup.
+
+	// Share class instance as common test fixture.
+	public static function setUpBeforeClass() {
 		// Issue #36 Cutout.
 		// See: https://github.com/NathanGibbs3/BASE/issues/36
 		$PHPV = GetPHPV();
 		$PSM = getenv('SafeMode');
 		if (version_compare($PHPV, '5.4', '<') && $PSM == 1){
-			$this->markTestSkipped();
-		}else{
-			$sc = DIRECTORY_SEPARATOR;
-			$file = 'readTestFail.txt';
-			$Testfile = "$BASE_path$sc" . "custom$sc$file";
-			if ( posix_getuid() != 1000 ){ // Swith UID to test Read Failure.
-				posix_setuid(1000);
-			}
-			$expected = -2;
-			if (
-				version_compare($PHPV, '5.1.5', '<')
-				&& ini_get("safe_mode") == true
-			){ // Safe Mode Cutout Return Value
-				$expected = 1;
-			}
-			$this->assertEquals(
-				$expected,
-				ChkAccess($Testfile),
-				'Unexpected return ChkAccess().'
-			);
+			self::markTestSkipped();
 		}
+	}
+	public static function tearDownAfterClass() {
+	}
+
+	// Tests go here.
+	public function testreturnChkAccessReadFail() {
+		GLOBAL $BASE_path;
+		$sc = DIRECTORY_SEPARATOR;
+		$file = 'readTestFail.txt';
+		$Testfile = "$BASE_path$sc" . "custom$sc$file";
+		$PHPV = GetPHPV();
+		if ( posix_getuid() != 1000 ){ // Swith UID to test Read Failure.
+			posix_setuid(1000);
+		}
+		$expected = -2;
+		if (
+			version_compare($PHPV, '5.1.5', '<')
+			&& ini_get("safe_mode") == true
+		){ // Safe Mode Cutout Return Value
+			$expected = 1;
+		}
+		$this->assertEquals(
+			$expected,
+			ChkAccess($Testfile),
+			'Unexpected return ChkAccess().'
+		);
 	}
 
 	// Add code to a function if needed.
